4d0b90d310e7c9d78bc4a4d0d1b863de44384c37
talafarael <127351345+talafarael@users.noreply.github.com>
2024-09-26 14:13:31 +0300

    fix 3and 7

--- 3.cpp
+++ 3.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-
+int sum100(int i);
 int main()
 {
   for(int i=0;i<10;i++){
       std::cout<<i+1<<std::endl;
       
   }
-  
+  int sum2=sum100(0);
+std::cout<<sum2<<std::endl;
   int a=0;
-  while(a!=100){a++;
-      std::cout<<a<<std::endl;
-      
-  }
+  int sum=0;
+  while(a!=100){
+        a++;
+    sum+=a;
+}
+  std::cout<<sum<<std::endl;
   int whileDo=0;
   do{  whileDo+=2;
       std::cout<<whileDo<<std::endl;
@@ -19,4 +22,9 @@ int main()
       
   }while(whileDo!=20);
   
+}
+int sum100(int i){
+    if(i==100){
+      return 100;}
+    return sum100(++i)+i;
 }
--- 7.cpp
+++ 7.cpp
@@ -3,10 +3,16 @@
 int main()
 {
     int knife=5;
+    double priceKnife=2.3;
     int cup=100;
+    double priceCup=2.2;
     int money=0;
+    double priceMoney=2.2;
     std::cout<<"knife:"<<knife<<std::endl;
+    std::cout<<"Price knife:"<<priceKnife<<std::endl;
     std::cout<<"cup:"<<cup<<std::endl;
+    std::cout<<"price cup:"<<priceCup<<std::endl;
 std::cout<<"money:"<<money<<std::endl;
-std::cout<<"sum"<<cup+knife+money<<std::endl;
+std::cout<<"money:"<<priceMoney<<std::endl;
+std::cout<<"sum"<<cup*priceCup+knife*priceKnife+money*priceMoney<<std::endl;
 }
